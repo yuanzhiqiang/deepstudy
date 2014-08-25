6e0b0c7a311f9df40bc8e484ee54c94e7ae46ba5
yuanzhiq <yuanzhiq@192.168.1.105>
2014-08-25 22:13:05 +0800

    socket

--- src/com/yuanzq/socket/serilizable/VoteMsgBinSerilizable.java
+++ src/com/yuanzq/socket/serilizable/VoteMsgBinSerilizable.java
@@ -1,21 +1,65 @@
 package com.yuanzq.socket.serilizable;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import com.yuanzq.socket.bookexample.chapter3.VoteMsg;
 
 public class VoteMsgBinSerilizable implements VoteMsgSerilizable {
 
+	public static final int MIN_WIRE_LENGTH = 4;
+	public static final int MAX_WIRE_LENGTH = 16;
+	public static final int MAGIC = 0x5400;
+	public static final int MAGIC_MASK = 0xfc00;
+	public static final int MAGIC_SHIFT = 8;
+	public static final int RESPONSE_FLAG = 0x0200;
+	public static final int INQUIRE_FLAG = 0x0100;
+
 	@Override
 	public byte[] serilizable(VoteMsg msg) throws IOException {
-		// TODO Auto-generated method stub
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(byteStream); // converts
+		short magicAndFlags = MAGIC;
+	    if (msg.isInquiry()) {
+	      magicAndFlags |= INQUIRE_FLAG;
+	    }
+	    if (msg.isResponse()) {
+	      magicAndFlags |= RESPONSE_FLAG;
+	    }
+	    out.writeShort(magicAndFlags);
+	    out.writeShort((short) msg.getCandidateID());
+	    if(msg.isResponse()){
+	    	out.writeLong(msg.getVoteCount());
+	    }
 		return null;
 	}
 
 	@Override
 	public VoteMsg unserilizable(byte[] input) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		boolean isInquiry = false; // true if inquiry; false if vote
+		boolean isResponse = false;// true if response from server
+		int candidateID; // in [0,1000]
+		long voteCount = 0; // nonzero only in response
+		ByteArrayInputStream bais = new ByteArrayInputStream(input);
+		DataInputStream dis = new DataInputStream(bais);
+		short magic = dis.readShort();
+		if((magic & MAGIC_MASK) != MAGIC){
+			throw new RuntimeException("error");
+		}
+		if((magic & INQUIRE_FLAG) == INQUIRE_FLAG){
+			isInquiry = true;
+		}
+		if((magic & RESPONSE_FLAG) == RESPONSE_FLAG){
+			isResponse = true;
+		}
+		candidateID = dis.readShort();
+		if(isResponse){
+			voteCount = dis.readLong();
+		}
+		return new VoteMsg(isResponse, isInquiry, candidateID, voteCount);
 	}
 
 }
--- src/com/yuanzq/socket/serilizable/VoteMsgTextSerilizable.java
+++ src/com/yuanzq/socket/serilizable/VoteMsgTextSerilizable.java
@@ -1,5 +1,6 @@
 package com.yuanzq.socket.serilizable;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,21 +8,58 @@ import com.yuanzq.socket.bookexample.chapter3.VoteMsg;
 
 public class VoteMsgTextSerilizable implements VoteMsgSerilizable {
 
+	public static final String MAGIC = "Voting";
+	public static final String VOTESTR = "v";
+	public static final String INQSTR = "i";
+	public static final String RESPONSESTR = "R";
+
+	public static final String CHARSETNAME = "US-ASCII";
+	public static final String DELIMSTR = " ";
+	public static final int MAX_WIRE_LENGTH = 2000;
+
 	@Override
 	public byte[] serilizable(VoteMsg msg) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		String msgString = MAGIC + DELIMSTR
+				+ (msg.isInquiry() ? INQSTR : VOTESTR) + DELIMSTR
+				+ (msg.isResponse() ? RESPONSESTR + DELIMSTR : "")
+				+ Integer.toString(msg.getCandidateID()) + DELIMSTR
+				+ Long.toString(msg.getVoteCount());
+		byte data[] = msgString.getBytes(CHARSETNAME);
+		return data;
+
 	}
 
 	@Override
 	public VoteMsg unserilizable(byte[] input) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		boolean isInquiry; // true if inquiry; false if vote
+		boolean isResponse;// true if response from server
+		int candidateID; // in [0,1000]
+		long voteCount; // nonzero only in response
+		ByteArrayInputStream bis = new ByteArrayInputStream(input);
+		Scanner scanner = new Scanner(bis);
+		String token = scanner.next();
+		if (!token.equals(MAGIC)) {
+			throw new RuntimeException("error");
+		}
+		token = scanner.next();
+		if (token.equals(INQSTR)) {
+			isInquiry = true;
+		}else{
+			isInquiry = false;
+		}
+		token = scanner.next();
+		if(RESPONSESTR.equals(token)){
+			isResponse = true;
+			token = scanner.next();
+		}else{
+			isResponse = false;
+		}
+		candidateID = Integer.parseInt(token);
+		token = scanner.next();
+		voteCount = Integer.parseInt(token);
+		return new VoteMsg(isResponse, isInquiry, candidateID, voteCount);
 	}
 
-	
-	
-	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		double d = scanner.nextDouble();
