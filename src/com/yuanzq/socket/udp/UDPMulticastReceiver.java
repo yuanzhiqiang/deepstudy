660f69961b3d3e82c748fe250012386a17fc6f07
yuanzq <yuanzhiqiang@yourmall.com>
2014-08-29 09:33:14 +0800

    ddd

--- src/com/yuanzq/socket/udp/UDPMulticastReceiver.java
+++ src/com/yuanzq/socket/udp/UDPMulticastReceiver.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class UDPMulticastReceiver {
 
 	
-	
+	//aaa
 	public static void main(String[] args) throws Exception {
 		int MAXSIZE = 1024;
 		byte[] buf = new byte[MAXSIZE];
